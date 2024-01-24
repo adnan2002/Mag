62729572b68d50a92b9f5271ae32374acb9f626f
Adnan <adnanm2002@outlook.com>
2024-01-25 02:18:17 +0300

    search first step

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController , IonSearchbar} from '@ionic/angular';
+import { ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
-export class SearchPage implements OnInit {
+export class SearchPage implements OnInit, AfterViewInit {
 
   constructor(private nav:NavController) { }
+
+  @ViewChild('searchbar', {static: false}) searchbar ?: IonSearchbar;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.searchbar?.setFocus();
+    }, 150);
+
+      
+  }
+
+
 
   goBack(){
     this.nav.back();
   }
 
   ngOnInit() {
+  }
+
+
+  search(ev:any){
+
   }
 
 }
